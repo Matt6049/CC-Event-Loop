5d3d4df141dcc222ad8a59b785a8c46528a6cd54
Matt6049 <mati6049@gmail.com>
2026-07-24 18:12:42 +0200

    feat: Create custom event type following the publisher/subscriber model

---
+++ CustomEvents.lua
@@ -0,0 +1,31 @@
+local function getEventTable()
+    local Event = {
+        fire = function(self, ...) 
+            for handler, _ in pairs(self.subscribers) do
+                handler(...);
+            end
+        end,
+
+        subscribe = function(self, handler) 
+            self.subscribers[handler] = true;
+        end,
+
+        unsubscribe = function(self, handler) 
+            self.subscribers[handler] = nil;
+        end
+    }
+
+    function Event:new()
+        local instance = setmetatable({
+            subscribers = {}
+        }, {
+            __index = Event
+        });
+
+        return instance;
+    end
+
+    return Event;
+end
+
+return getEventTable();
