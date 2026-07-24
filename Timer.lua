e6259934d7dce0488d0391d818ece84e495c6249
Matt6049 <mati6049@gmail.com>
2026-07-24 18:15:41 +0200

    feat: Create custom timer impl to free up the CC Timer event for the main loop

---
+++ Timer.lua
@@ -0,0 +1,37 @@
+local Event = require("CustomEvents");
+local RunningTimers = {}
+
+
+local Timer = {
+    interval = 0.05,
+    timeRemaining = 0,
+    looping = false,
+    onElapsed = nil,
+
+    start = function(self)
+        self.timeRemaining = self.interval;
+        RunningTimers[self] = true;
+    end,
+
+    cancel = function(self)
+        self.timeRemaining = 0;
+        RunningTimers[self] = nil;
+    end,
+
+    getRunning = function()
+        return RunningTimers;
+    end
+}
+
+function Timer:new(props)
+    local instance = setmetatable(props or {}, {
+        __index = Timer
+    });
+    instance.onElapsed = Event:new();
+    instance.onElapsed:subscribe(function()
+        RunningTimers[instance] = nil
+    end);
+    return instance;
+end
+
+return Timer;
