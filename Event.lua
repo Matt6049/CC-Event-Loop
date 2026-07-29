1df87aab1a345d11004af78a96976498961800c6
Matt6049 <mati6049@gmail.com>
2026-07-30 01:39:58 +0200

    refactor: change event impl to dense array
    after days of testing and benchmarks, this achieves a 5x improvement in
    iteration speed

--- Event.lua
+++ Event.lua
@@ -1,31 +1,48 @@
-local Event = {
-    subscribers = {},
+local min = -math.pow(2, 52);
+local _count = {};
+local _maxId = {};
 
-    fire = function(self, ...) 
-        for handler, _ in pairs(self.subscribers) do
-            handler(...);
+--CONVENTION:
+--Handlers are stored in index-1. Their ID is stored in index.
+--ID to Index mappings are stored at min+ID. This is a sparse array. 
+--maxID is used to guarantee a unique ID for each handler for quick removal.
+--Count includes handlers and their IDs. 
+
+--Yes, empty table keys are better than string keys. No, I don't know why.
+--This somehow manages a 5x performance improvement as compared to hashsets.
+local Event = {
+    fire = function(self, ...)
+        for i=1, self[_count], 2 do
+            self[i](...);
         end
     end,
 
-    subscribe = function(self, handler) 
-        self.subscribers[handler] = true;
+    subscribe = function(self, handler)
+        local count, maxId = self[_count]+2, self[_maxId]+1;
+        self[_count], self[_maxId] = count, maxId;
+        self[count-1], self[count] = handler, maxId;
+        self[maxId] = count;
+        return maxId;
     end,
 
-    unsubscribe = function(self, handler) 
-        self.subscribers[handler] = nil;
+    unsubscribe = function(self, id)
+        local count = self[_count];
+        local index = self[id];
+        if(index ~= count) then
+            local swapId = self[count];
+            self[index-1], self[index] = self[count-1], self[count];
+            self[swapId] = index;
+        end
+        self[count], self[count-1], self[id] = nil, nil, nil;
+        self[_count] = count - 2;
     end
-    
 }
 
 function Event:new()
-    local instance = setmetatable({
-        subscribers = {}
-    }, {
-        __index = self
-    });
-
-    return instance;
+    return setmetatable({
+        [_maxId] = min,
+        [_count] = 0
+    }, {__index=Event});
 end
-
 
 return Event;
