63f9b303b33f99e0260796a6948698f8c6be8d90
Matt6049 <mati6049@gmail.com>
2026-07-30 02:40:31 +0200

    refactor: add protection against mutating the handler list while firing.

--- Event.lua
+++ Event.lua
@@ -1,5 +1,6 @@
 local min = -math.pow(2, 52);
 local _count = {};
+local _unsubCount = {};
 local _maxId = {};
 
 --CONVENTION:
@@ -7,12 +8,18 @@ local _maxId = {};
 --ID to Index mappings are stored at min+ID. This is a sparse array. 
 --maxID is used to guarantee a unique ID for each handler for quick removal.
 --Count includes handlers and their IDs. 
+--Unsub queue starts at -1 and counts down.
 
 --Yes, empty table keys are better than string keys. No, I don't know why.
 --This somehow manages a 5x performance improvement as compared to hashsets.
 local Event = {
     fire = function(self, ...)
-        for i=1, self[_count], 2 do
+        local unsubCount = self[_unsubCount];
+        if(unsubCount > 0) then
+            self:clearUnsubQueue(unsubCount);
+        end
+
+        for i=self[_count]-1, 1, -2 do
             self[i](...);
         end
     end,
@@ -26,22 +33,38 @@ local Event = {
     end,
 
     unsubscribe = function(self, id)
-        local count = self[_count];
         local index = self[id];
-        if(index ~= count) then
-            local swapId = self[count];
-            self[index-1], self[index] = self[count-1], self[count];
-            self[swapId] = index;
+        if(index) then
+            self[id] = nil;
+            local unsubCount = self[_unsubCount];
+            self[_unsubCount] = unsubCount + 1;
+            self[0-unsubCount-1] = index;
         end
-        self[count], self[count-1], self[id] = nil, nil, nil;
-        self[_count] = count - 2;
+    end,
+
+    clearUnsubQueue = function(self, unsubCount)
+        local count = self[_count];
+        local index, swapId = nil, nil;
+        for i=-1, -unsubCount, -1 do
+            index = self[i];
+            if(index ~= count) then
+                swapId = self[count];
+                self[index-1], self[index] = self[count-1], self[count];
+                self[swapId] = index;
+            end
+            self[count], self[count-1] = nil, nil
+            count = count - 2;
+        end
+        self[_count] = count;
+        self[_unsubCount] = 0;
     end
 }
 
 function Event:new()
     return setmetatable({
         [_maxId] = min,
-        [_count] = 0
+        [_count] = 0,
+        [_unsubCount] = 0,
     }, {__index=Event});
 end
 
