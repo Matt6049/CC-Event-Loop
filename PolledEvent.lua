1cdf98b29b217055f0d43b5705f6aa74804aa536
Matt6049 <mati6049@gmail.com>
2026-08-01 00:54:21 +0200

    refactor: complete rework of polledevents to use lazy subscription and reduce reliance on closures, while allowing trigger event and poller mutation

--- PolledEvent.lua
+++ PolledEvent.lua
@@ -1,17 +1,98 @@
 local NativeEvents = require("NativeEvents");
 local Event = require("Event");
 
-local PolledEvent = {};
+local _type = {};
+local _subscribedEvents, _subscribedTables, _unsubscribed = {}, {}, {};
+local _pollHandler, _indexNext = {}, {};
 
-function PolledEvent:new(parent, poller)
-    local weakRefs = setmetatable({parent, Event:new()}, {__mode='v'});
-    local func = nil;
-    func = function()
-        if(weakRefs[1] and weakRefs[2]) then poller(weakRefs[1], weakRefs[2]);
-        else NativeEvents.tick:unsubscribe(func); end
-    end
-    NativeEvents.tick:subscribe(func);
-    return weakRefs[2];
+local function unwrap(event)
+    event.once = nil;
+    event.subscribe = nil;
+    local type = event[_type];
+    local unsubscribed = type[_unsubscribed];
+    local parent = unsubscribed[event];
+    unsubscribed[event] = nil;
+
+    local events, tables = type[_subscribedEvents], type[_subscribedTables];
+    local index = type[_indexNext];
+    type[_indexNext] = index + 1;
+    events[index] = event;
+    tables[index] = parent;
 end
 
-return PolledEvent;
+local function onceWrapper(event, handler)
+    unwrap(event);
+    event:once(handler);
+end
+
+local function subscribeWrapper(event, handler)
+    unwrap(event);
+    event:subscribe(handler);
+end
+
+
+local PolledEventType = {
+    newEvent = function(self, parent)
+        local event = Event.new();
+        self[_unsubscribed][event] = parent;
+        event[_type] = self;
+        event.subscribe = subscribeWrapper;
+        event.once = onceWrapper;
+        return event;
+    end,
+};
+
+local function getPollHandler(self)
+    return function()
+        local pollPredicate = self.pollPredicate;
+        local i = 1;
+        local len = self[_indexNext]-1;
+
+        local events = self[_subscribedEvents];
+        local tables = self[_subscribedTables];
+        local event = events[i];
+        local table = tables[i];
+        while event do
+            if(table) then
+                pollPredicate(event, table);
+                i = i+1;
+                event = events[i]; table = tables[i];
+            else
+                --todo: event destroy here potentially
+                if(i ~= len) then
+                    event = events[len]; table = tables[len];
+                    events[i] = event; tables[i] = table;
+                else
+                    event = nil;
+                end
+                events[len] = nil; tables[len] = nil;
+                len = len-1;
+            end
+        end
+        self.triggerEvent:once(self[_pollHandler]);
+    end
+end
+
+local meta = {__index = PolledEventType};
+local weakTable = {__mode='kv'};
+local weakVal = {__mode='v'};
+---Creates a new polled event type.
+---@param triggerEvent Event Event that triggers a poll.
+---@param pollPredicate fun(event, table) Predicate that should fire the event if the provided table meets the conditions.
+---@return table
+function PolledEventType.new(pollPredicate, triggerEvent)
+    local instance = setmetatable({
+        [_subscribedEvents] = {},
+        [_subscribedTables] = setmetatable({}, weakVal),
+        [_unsubscribed] = setmetatable({}, weakTable),
+        [_indexNext] = 1,
+        triggerEvent = triggerEvent or NativeEvents.tick,
+        pollPredicate = pollPredicate,
+    }, meta);
+    local pollHandler = getPollHandler(instance);
+    instance[_pollHandler] = pollHandler;
+    triggerEvent:once(pollHandler);
+    return instance;
+end
+
+return PolledEventType;
