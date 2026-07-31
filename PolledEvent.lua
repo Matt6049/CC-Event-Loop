4a92fe991af2d940397c36acd244bde5d8846d1d
Matt6049 <mati6049@gmail.com>
2026-08-01 01:11:43 +0200

    bugfix: function wiring, remove last element if parent is nil, rename table local var to parent

--- PolledEvent.lua
+++ PolledEvent.lua
@@ -30,7 +30,9 @@ local function subscribeWrapper(event, handler)
     event:subscribe(handler);
 end
 
-
+---@class PolledEventType
+---@field triggerEvent Event Event that triggers a poll.
+---@field pollPredicate fun(event, weakTable) Predicate that should fire the event if its parent meets the conditions.
 local PolledEventType = {
     newEvent = function(self, parent)
         local event = Event.new();
@@ -42,6 +44,9 @@ local PolledEventType = {
     end,
 };
 
+---Creates a poll handler closure that will 
+---@param self PolledEventType
+---@return function 
 local function getPollHandler(self)
     return function()
         local pollPredicate = self.pollPredicate;
@@ -51,17 +56,17 @@ local function getPollHandler(self)
         local events = self[_subscribedEvents];
         local tables = self[_subscribedTables];
         local event = events[i];
-        local table = tables[i];
+        local parent = tables[i];
         while event do
-            if(table) then
-                pollPredicate(event, table);
+            if(parent) then
+                pollPredicate(event, parent);
                 i = i+1;
-                event = events[i]; table = tables[i];
+                event = events[i]; parent = tables[i];
             else
                 --todo: event destroy here potentially
                 if(i ~= len) then
-                    event = events[len]; table = tables[len];
-                    events[i] = event; tables[i] = table;
+                    event = events[len]; parent = tables[len];
+                    events[i] = event; tables[i] = parent;
                 else
                     event = nil;
                 end
@@ -73,13 +78,25 @@ local function getPollHandler(self)
     end
 end
 
+---Wraps PolledEventType:newEvent to only start polling when it creates its first event.
+---@param self PolledEventType
+---@param parent table 
+local function newEventWrapper(self, parent)
+    self.newEvent = nil;
+    local pollHandler = getPollHandler(self);
+    self[_pollHandler] = pollHandler;
+    self.triggerEvent:once(pollHandler);
+
+    self:newEvent(parent);
+end
+
 local meta = {__index = PolledEventType};
 local weakTable = {__mode='kv'};
 local weakVal = {__mode='v'};
 ---Creates a new polled event type.
 ---@param triggerEvent Event Event that triggers a poll.
----@param pollPredicate fun(event, table) Predicate that should fire the event if the provided table meets the conditions.
----@return table
+---@param pollPredicate fun(event, parent) Predicate that should fire the event if its parent meets the conditions.
+---@return PolledEventType
 function PolledEventType.new(pollPredicate, triggerEvent)
     local instance = setmetatable({
         [_subscribedEvents] = {},
@@ -88,10 +105,8 @@ function PolledEventType.new(pollPredicate, triggerEvent)
         [_indexNext] = 1,
         triggerEvent = triggerEvent or NativeEvents.tick,
         pollPredicate = pollPredicate,
+        newEvent = newEventWrapper
     }, meta);
-    local pollHandler = getPollHandler(instance);
-    instance[_pollHandler] = pollHandler;
-    triggerEvent:once(pollHandler);
     return instance;
 end
 
